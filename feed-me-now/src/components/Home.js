b28f3778398489687f9c16ee35dd86ca066ac284
Jesse Ma <j.masonoma17@gmail.com>
2018-10-17 21:10:43 -0700

    Components folder: Add new hierarchical components

---
+++ feed-me-now/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Header from './Header';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="Home">
+                <Header />
+            </div>
+        )
+    }
+}
+
+export default Home;
---
+++ feed-me-now/src/components/Preview.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Preview extends Component {
+    render() {
+        return (
+            <div className="Preview">
+
+            </div>
+        )
+    }
+}
+
+export default Preview;
