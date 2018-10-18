13f8593b8bae7f1d5fd7d3c530af8e45c8bbe329
Jesse Ma <j.masonoma17@gmail.com>
2018-10-17 21:34:23 -0700

    Preview.js: Add logo

--- feed-me-now/src/components/Preview.js
+++ feed-me-now/src/components/Preview.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+const logoPath = process.env.PUBLIC_URL + '/assets/imgs/logo.png';
+
 class Preview extends Component {
     render() {
         return (
             <div className="Preview">
-
+                <img src={`${logoPath}`} alt="Brand logo" />
             </div>
         )
     }
