ea22e750c8067460b00a4ef8ff71dcf098b11ea4
Jesse Ma <j.masonoma17@gmail.com>
2018-11-02 18:48:55 -0700

    Server.js: Have server listen to port 8000, update package dependency with axios to make http requests to yelp fusion API

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+
+const yelpKey = require('./yelpConfig.js')['yelpAPIKey'];
+
+const port = 8000;
+
+app.get('/getrestaurant', (req, res, next) => {
+
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
