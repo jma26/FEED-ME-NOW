b63d6688e6f65b1fa68a00426941439a8e7e559f
Jesse Ma <j.masonoma17@gmail.com>
2018-11-02 23:09:33 -0700

    Server.js: Set config defaults for axios globally and a http get request to yelp api

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,25 @@ const axios = require('axios');
 
 const yelpKey = require('./yelpConfig.js')['yelpAPIKey'];
 
+// Set config defaults for axios globally
+axios.defaults.baseURL = "https://api.yelp.com/v3/";
+axios.defaults.headers.common['Authorization'] = `Bearer ${yelpKey}`;
+
 const port = 8000;
 
 app.get('/getrestaurant', (req, res, next) => {
-
+    axios.get('businesses/search', {
+        params: {
+            term: 'restaurants',
+            location: 'Santa Rosa, CA'
+        }
+    })
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
 });
 
 app.listen(port, () => {
