a94ccbb63c90eb4f158826426096e49713d81390
Jesse Ma <j.masonoma17@gmail.com>
2018-11-06 16:24:38 -0800

    App.js: Fix typo on component attribute on /home route

--- feed-me-now/src/App.js
+++ feed-me-now/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
             <div className="App">
                 <Route exact path="/" component={ Preview } />
                 <Switch>
-                    <Route path="/home" compoonent={ Home } />
+                    <Route path="/home" component={ Home } />
                 </Switch>
             </div>
         );
