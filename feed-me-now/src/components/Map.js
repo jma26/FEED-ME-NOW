fe7c441373d83feaff5d80ec716fa35337db2748
Jesse Ma <j.masonoma17@gmail.com>
2018-11-09 01:51:04 -0800

    Map.js: Add new lifecycles and loadDirections() to display route directions on map

--- feed-me-now/src/components/Map.js
+++ feed-me-now/src/components/Map.js
@@ -3,22 +3,53 @@ import React, { Component } from 'react';
 class Map extends Component {
     constructor(props) {
         super(props);
-        this.state = { ...props };
+        this.state = {
+            userLocation: null
+         };
+         this.loadDirections=this.loadDirections.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.state.userLocation !== nextProps.userLocation) {
+            this.setState({
+                userLocation: nextProps.userLocation
+            }, this.loadDirections)
+        }
+    }
+
+    loadDirections() {
+        console.log(this.state.userLocation);
+        window.L.mapquest.directions().route({
+            start: `${this.state.userLocation}`,
+            end: '38.447536, -122.701134'
+        })
     }
 
     componentDidMount() {
         window.L.mapquest.key = process.env.REACT_APP_MAPQUEST_API_KEY;
         window.L.mapquest.map('map', {
-            center: this.state.center,
-            layers: window.L.mapquest.tileLayer(this.state.baseLayer),
-            zoom: this.state.zoom,
+            center: this.props.center,
+            layers: window.L.mapquest.tileLayer(this.props.baseLayer),
+            zoom: this.props.zoom,
         });
+        let directions = window.L.mapquest.directions();
+        directions.setLayerOptions({
+            startMarker: {
+                draggable: false
+            },
+            endMarker: {
+                draggable: false
+            },
+            routeRibbon: {
+                draggable: false
+            }
+        })
     }
 
     render() {
         const mapStyle = {
-            height: this.state.height,
-            width: this.state.width
+            height: this.props.height,
+            width: this.props.width
         }
         return (
             <div className="Map">
