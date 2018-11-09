63479d76910f704f4d204f333ec131bc49a6bfae
Jesse Ma <j.masonoma17@gmail.com>
2018-11-08 22:13:55 -0800

    Map.js: Remove updateDimensions() and affiliated functions

--- feed-me-now/src/components/Map.js
+++ feed-me-now/src/components/Map.js
@@ -5,26 +5,8 @@ class Map extends Component {
         super(props);
         this.state = { ...props };
     }
-    updateDimensions() {
-        const height = window.innerWidth >= 700 ? window.innerHeight : 700
-        const width = window.innerWidth >= 400 ? window.innerWidth : 400
-        this.setState({
-            height: height,
-            width: width,
-        })
-        console.log(width, height);
-    }
-
-    componentWillMount() {
-        this.updateDimensions();
-    }
-
-    componentWillUnmount() {
-        window.addEventListener("resize", this.updateDimensions.bind(this))
-    }
 
     componentDidMount() {
-        window.addEventListener("resize", this.updateDimensions.bind(this))
         window.L.mapquest.key = process.env.REACT_APP_MAPQUEST_API_KEY;
         window.L.mapquest.map('map', {
             center: this.state.center,
