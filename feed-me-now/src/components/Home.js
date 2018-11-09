81012ce24f5c73eeefda2d91618f0912c4e70d2b
Jesse Ma <j.masonoma17@gmail.com>
2018-11-09 01:50:15 -0800

    Home.js: Move ternary operation for geolocation to lifecycle, update to userLocation prop

--- feed-me-now/src/components/Home.js
+++ feed-me-now/src/components/Home.js
@@ -10,11 +10,15 @@ class Home extends Component {
         this.state = {
             geolocation: props.location.state.isSharingGeolocation
         }
+        this.displayLocationInfo = this.displayLocationInfo.bind(this);
     }
 
     displayLocationInfo(position) {
         const lng = position.coords.longitude;
         const lat = position.coords.latitude;
+        this.setState({
+            userLocation: `${lat}, ${lng}`
+        })
         console.log(`User's geolocation is ${lng}, ${lat}`);
         // http request to server
         axios.post('http://localhost:8000/getrestaurant', {
@@ -26,29 +30,33 @@ class Home extends Component {
 
     getUserLocation() {
         // Check if API is available on user's browsers
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(this.displayLocationInfo);
-        }
+        navigator.geolocation.getCurrentPosition(this.displayLocationInfo);
     }
 
     defaultLocation() {
         const defaultLng = -122.083855;
         const defaultLat = 37.386051;
+        this.setState({
+            userLocation: `${defaultLat}, ${defaultLng}`
+        })
         console.log(`Default geolocation is ${defaultLng}, ${defaultLat}`);
     }
 
-    render() {
+    componentDidMount() {
         navigator.geolocation && this.state.geolocation ? this.getUserLocation() : this.defaultLocation();
+    }
+
+    render() {
         return (
             <div className="Home">
                 <Header />
                 <Map
+                    userLocation={this.state.userLocation}
                     height={'100vh'}
                     width={'100%'}
                     center={[37.7749, -122.4194]}
                     baseLayer={'light'}
                     zoom={14}
-                    apiKey={process.env.REACT_APP_MAPQUEST_API_KEY}
                 />
             </div>
         )
