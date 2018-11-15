c2e2503524721c95bdf93105a13990ce2344c605
Jesse Ma <j.masonoma17@gmail.com>
2018-11-14 21:21:22 -0800

    Preview.js: Add animations to content with animate.css

--- feed-me-now/src/components/Preview.js
+++ feed-me-now/src/components/Preview.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import './Preview.css';
 import { Link } from 'react-router-dom';
 
-const logoPath = process.env.PUBLIC_URL + '/assets/imgs/logo.png';
+const logoPath = process.env.PUBLIC_URL + '/assets/imgs/logo.svg';
 
 class Preview extends Component {
     render() {
         return (
             <div className="Preview">
-                <h1 className="Preview__intro"> FEED ME NOW is an awesome web app for those who can't decide where to eat. </h1> 
-                <h1 className="Preview__intro--mobile-content"> FEED ME NOW </h1>
-                <h2 className="Preview__content"> Click accept to share your location or decline to manually input it. </h2>
-                <img src={`${logoPath}`} alt="Brand logo" />
+                <h1 className="Preview__intro animated bounceInDown"> FEED ME NOW is an awesome web app for those who can't decide where to eat. </h1> 
+                <h1 className="Preview__intro--mobile-content animated bounceInDown"> FEED ME NOW </h1>
+                <h2 className="Preview__content animated bounceInUp"> Click accept to share your location or decline to manually input it. </h2>
+                <div className="Preview__logo-container animated fadeIn">
+                    <img src={`${logoPath}`} alt="Brand logo" className="Preview__logo" />
+                </div>
                 <div className="Preview__btns">
                     <button className="Preview__btn--green preview-btns">
                         <Link to={{pathname: "/home", state: {isSharingGeolocation: true} }} style={{textDecoration: 'none', color: '#FFF'}}> Accept </Link>
