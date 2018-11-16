dac5b8ef41182f6b95a2e8e6f01d47e33650d37d
Jesse Ma <j.masonoma17@gmail.com>
2018-11-16 01:44:43 -0800

    Loading.js: Initial commit

---
+++ feed-me-now/src/components/Loading.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Loading extends Component {
+    render() {
+        return (
+            <div className="Loading">
+                <h1>Loading.....</h1>
+            </div>
+        )
+    }
+}
+
+export default Loading;
