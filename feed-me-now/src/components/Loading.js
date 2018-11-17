15791ac9c21f18f8c47b66eb85ff8d0d958ef2e4
Jesse Ma <j.masonoma17@gmail.com>
2018-11-17 01:15:15 -0800

    Loading.js: Add loading spinner

--- feed-me-now/src/components/Loading.js
+++ feed-me-now/src/components/Loading.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import './Loading.css';
 
 class Loading extends Component {
     render() {
         return (
             <div className="Loading">
-                <h1>Loading.....</h1>
+                <div className="Loading__spinner">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
         )
     }
