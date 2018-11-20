a45a9e9b17b90e60b8fd5dcc8d217c46010f68ff
Jesse Ma <j.masonoma17@gmail.com>
2018-11-19 21:06:18 -0800

    Footer.js: Add footer content and social links

--- feed-me-now/src/components/Footer.js
+++ feed-me-now/src/components/Footer.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { Link } from 'react-router-dom';
 import './Footer.css';
 
@@ -10,11 +12,19 @@ class Footer extends Component {
     render() {
         return (
             <div className="Footer">
-                <button className="footer__btn blk-bg animated slideInRight">
+                <button className="footer__dislike-btn blk-bg animated slideInRight">
                     <Link to={{pathname: "/home", state: {isSharingGeolocation: this.props.isSharingGeolocation} }} style={{textDecoration: 'none', color: '#FFF'}}> No, I don't want that </Link>
                 </button>
-                <div className="footer__links">
-                    <p>Created by Jesse Ma</p>
+                <div className="footer-container">
+                    <div className="footer-container__social">
+                        <a href="https://github.com/jma26" target="_blank" className="footer-container__social-link"><FontAwesomeIcon icon={faGithub} size="lg" 
+                        className="footer-container__icons social-links--hyperlink" /></a>
+                        <a href="https://www.linkedin.com/in/jesse-ma-b2522b84/" target="_blank" className="footer-container__social-link"><FontAwesomeIcon icon={faLinkedin} size="lg" className="footer-container__icons social-links--hyperlink" /></a>
+                        <p className="footer-container__content">Jesse Ma</p>
+                    </div>
+                    <button className="footer-container__location-btn">
+                        <Link to={{pathname: "/location", state: {isSharingGeolocation: false} }} style={{textDecoration: 'none'}}>Change Location</Link>
+                    </button>
                 </div>
             </div>
         )
