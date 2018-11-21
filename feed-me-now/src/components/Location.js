f11d29a66be31978d344002d6b603a18725ae179
Jesse Ma <j.masonoma17@gmail.com>
2018-11-21 00:05:22 -0800

    Location.js: Add link to /home and change userLocation state onChange()

--- feed-me-now/src/components/Location.js
+++ feed-me-now/src/components/Location.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 
 class Location extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            'userLocation': ''
+        }
     }
+
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
     render() {
         return (
             <div className="Location">
+                <h2> Enter your location </h2>
+                <input type="text" name="userLocation" value={this.state.userLocation} onChange={(e) => this.onChange(e)} />
+                <button className="location__location-btn">
+                    <Link to={{pathname: "/home", state: {isSharingGeolocation: false, isManuallyLocated: true} }}>Find new restaurant</Link>
+                </button>
             </div>
         )
     }
