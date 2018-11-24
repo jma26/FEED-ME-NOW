c1c1ec99c35c0a0d9845724993e70d47d4213157
Jesse Ma <j.masonoma17@gmail.com>
2018-11-23 22:01:57 -0800

    Home.js: Add if statement to load error component or not

--- feed-me-now/src/components/Home.js
+++ feed-me-now/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 
 import Header from './Header';
@@ -10,10 +11,10 @@ class Home extends Component {
         super(props);
         this.state = {
             geolocation: props.location.state.isSharingGeolocation,
-            manualLocationInfo: props.location.state.manualLocationInfo,
-            isManuallyLocated: props.location.state.isManuallyLocated,
             center: props.location.state.center,
-            userLocation: props.location.state.userLocation
+            userLocation: props.location.state.userLocation,
+            hasError: false,
+            errorMsg: ''
         }
         this.displayLocationInfo = this.displayLocationInfo.bind(this);
     }
@@ -27,16 +28,28 @@ class Home extends Component {
         })
         .then((response) => {
             console.log(response);
-            this.setState({
-                restaurant: {
-                    name: response.data.name,
-                    address: response.data.location.display_address,
-                    phone: response.data.restaurant_phone,
-                    rating: response.data.rating,
-                    coords: response.data.coordinates,
-                    url: response.data.url
-                },
-            })
+            if (response.data.error) {
+                console.log('Sorry, no restaurants found');
+                this.setState({
+                    hasError: true,
+                    errorMsg: response.data.error
+
+                })
+            } else {
+                this.setState({
+                    restaurant: {
+                        name: response.data.name,
+                        address: response.data.location.display_address,
+                        phone: response.data.restaurant_phone,
+                        rating: response.data.rating,
+                        coords: response.data.coordinates,
+                        url: response.data.url
+                    },
+                })
+            }
+        })
+        .catch((error) => {
+            console.log(error);
         }); 
     }
 
@@ -57,25 +70,6 @@ class Home extends Component {
         navigator.geolocation.getCurrentPosition(this.displayLocationInfo);
     }
 
-    geoCodeLocation() {
-        console.log(`Geocoding address....${this.props.location.state.manualLocationInfo}`);
-        const MAPQUEST_GEOCODING_API_URL = 'http://www.mapquestapi.com/geocoding/v1/address';
-        axios.get(`${MAPQUEST_GEOCODING_API_URL}?key=${process.env.REACT_APP_MAPQUEST_API_KEY}&location=${this.props.location.state.manualLocationInfo}`)
-        .then((response) => {
-            const geoCoordinates = response.data.results[0].locations[0].latLng;
-            this.getRestaurant(geoCoordinates.lat, geoCoordinates.lng);
-            this.setState({
-                userLocation: `${geoCoordinates.lat}, ${geoCoordinates.lng}`,
-                center: [geoCoordinates.lat, geoCoordinates.lng]
-            })
-            console.log(`${geoCoordinates.lat}, ${geoCoordinates.lng}`);
-
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    }
-
     reloadNewRestaurant() {
         this.getRestaurant(this.state.center[0], this.state.center[1]);
     }
@@ -93,9 +87,7 @@ class Home extends Component {
     }
 
     componentDidMount() {
-        if (this.props.location.state.isManuallyLocated) {
-            this.geoCodeLocation();
-        } else if (navigator.geolocation && this.state.geolocation) {
+        if (navigator.geolocation && this.state.geolocation) {
             this.getUserLocation();
         } else if (this.state.center && this.state.userLocation) {
             this.reloadNewRestaurant();
@@ -105,22 +97,29 @@ class Home extends Component {
     }
 
     render() {
+        var REDIRECT_COMPONENT, MAP_LOADING_COMPONENT;
+        if (this.state.hasError && this.state.errorMsg) {
+            REDIRECT_COMPONENT = <Redirect to={{
+                pathname: '/error',
+                state: {error: this.state.errorMsg}
+            }} />
+        } else {
+            MAP_LOADING_COMPONENT = this.state.center && this.state.userLocation ? <Map
+                isSharingGeolocation={this.state.geolocation}
+                restaurant={this.state.restaurant}
+                userLocation={this.state.userLocation}
+                height={'100vh'}
+                width={'100%'}
+                center={this.state.center}
+                baseLayer={'map'}
+                zoom={14}
+                /> : <Loading />
+        }
         return (
             <div className="Home">
                 <Header />
-                {/* Show loading component if data is loading */}
-                {
-                    this.state.center && this.state.userLocation ? <Map
-                        isSharingGeolocation={this.state.geolocation}
-                        restaurant={this.state.restaurant}
-                        userLocation={this.state.userLocation}
-                        height={'100vh'}
-                        width={'100%'}
-                        center={this.state.center}
-                        baseLayer={'map'}
-                        zoom={14}
-                    /> : <Loading />
-                }
+                {REDIRECT_COMPONENT}
+                {MAP_LOADING_COMPONENT}
             </div>
         )
     }
