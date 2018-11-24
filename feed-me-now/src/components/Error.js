1e586264feb9c04f7ef307abe93f6e640dc60c63
Jesse Ma <j.masonoma17@gmail.com>
2018-11-23 22:45:55 -0800

    Error.js: Display 204 with error content and link to /location

--- feed-me-now/src/components/Error.js
+++ feed-me-now/src/components/Error.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Error.css';
 
 class Error extends Component {
     constructor(props) {
@@ -7,7 +9,16 @@ class Error extends Component {
     render() {
         return (
             <div className="Error">
-                <h1>{this.props.location.state.error}</h1>
+                <div class="error__notfound">
+                    <div class="error__notfound-404">
+                        <h3>Oops!</h3>
+                        <h1><span>2</span><span>0</span><span>4</span></h1>
+                    </div>
+                    <h2 className="error__headline--color">{this.props.location.state.error}</h2>
+                    <button className="error__location-btn">
+                        <Link to={{pathname: "/location", state: {isSharingGeolocation: false}}} style={{textDecoration: 'none', color: 'black'}}>Click here to enter your location</Link>
+                    </button>
+                </div>
             </div>
         )
     }
